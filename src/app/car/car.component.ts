904267c8df36d6a21d8e3f8f691c7fdb9b7076f2
ss.kovalevskiy <ss.kovalevskiy@yandex.ru>
2017-11-11 19:23:14 +0300

    Добавлены два других варианта использования selector: как атрибута и класса div-элементта

--- src/app/car/car.component.ts
+++ src/app/car/car.component.ts
@@ -2,6 +2,12 @@ import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-car',
+  //Другой вариант использования селектора как атрибута другого тега. В данном случае в HTML-разметке вызывается через
+  //добавление аттрибута обычного тега, например <div app-car></div>
+  // selector: '[app-car]',
+  //Третий вариант использования селектора - через class элемента. В разметке выглядит как: <div class="app-car"></div>
+  //selector: '.app-car'
+
   // templateUrl: './cars.component.html',
   template: `
     <h2>Single car</h2>
