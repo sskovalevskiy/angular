c9df55e838a6c5e2951580655111f259360616a8
ss.kovalevskiy <ss.kovalevskiy@yandex.ru>
2017-11-11 18:44:44 +0300

    Change HTML view of car.component, add CSS rules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { MyComponent } from './my/my.component';
-import { NewmyComponent } from './newmy/newmy.component';
 import { CarsComponent } from './cars/cars.component';
 import { CarComponent } from './car/car.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MyComponent,
-    NewmyComponent,
     CarsComponent,
     CarComponent
   ],
