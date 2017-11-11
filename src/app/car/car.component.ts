06455507748ab399a782b47645d33674dd335665
ss.kovalevskiy <ss.kovalevskiy@yandex.ru>
2017-11-11 18:51:23 +0300

    Change car.component settings: templateUrl -> template; styleUrls -> styles, and add content into them

--- src/app/car/car.component.ts
+++ src/app/car/car.component.ts
@@ -1,13 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-car',
-  templateUrl: './car.component.html',
-  styleUrls: ['./car.component.css']
+  // templateUrl: './cars.component.html',
+  template: `
+    <h2>Single car</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur, sapiente?</p>
+  `,
+  // styleUrls: ['./car.component.css'],
+  styles: [`
+    h2 {
+      color: aqua;
+    }
+  `]
 })
 export class CarComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
