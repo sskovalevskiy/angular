777eebbf5dcfec5ed288eefd79add08362a07827
ss.kovalevskiy <ss.kovalevskiy@yandex.ru>
2017-11-11 15:50:19 +0300

    Create my.component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MyComponent } from './my/my.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyComponent
   ],
   imports: [
     BrowserModule
