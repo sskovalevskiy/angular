486f7d5e7d519825d59284edbd0112649bbc91f1
ss.kovalevskiy <ss.kovalevskiy@yandex.ru>
2017-11-15 04:18:16 +0300

    Пример создания собственной директивы

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { CarsComponent } from './cars/cars.component';
 import { CarComponent } from './car/car.component';
 import {FormsModule} from "@angular/forms";
 import { AddCarComponent } from './add-car/add-car.component';
+import {BackgroundDirective} from "./directives/background.directive";
 
 @NgModule({
   declarations: [
     AppComponent,
     CarsComponent,
     CarComponent,
-    AddCarComponent
+    AddCarComponent,
+    BackgroundDirective
   ],
   imports: [
     BrowserModule,
---
+++ src/app/directives/background.directive.ts
@@ -0,0 +1,15 @@
+import {Directive, ElementRef, OnInit} from "@angular/core";
+
+@Directive({
+  selector: '[appBackground]'
+})
+export class BackgroundDirective implements OnInit{
+
+  constructor(private element: ElementRef) {
+
+  }
+
+  ngOnInit(): void {
+    this.element.nativeElement.style.backgroundColor = 'violet';
+  }
+}
