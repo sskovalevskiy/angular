ac432fe0f304e9bfd8bbde402bf6cf71bc29e358
ss.kovalevskiy <ss.kovalevskiy@yandex.ru>
2017-11-15 03:33:15 +0300

    Примеры использования этапов жизненного цикла компонента

--- src/app/car/car.component.ts
+++ src/app/car/car.component.ts
@@ -1,16 +1,69 @@
-import {Component, ContentChild, ElementRef, Input} from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit, AfterViewChecked, AfterViewInit, Component, ContentChild, DoCheck, ElementRef, Input, OnChanges,
+  OnDestroy,
+  OnInit,
+  SimpleChanges
+} from '@angular/core';
 
 @Component({
   selector: 'app-car',
   templateUrl: './car.component.html',
   styleUrls: ['./car.component.css']
 })
-export class CarComponent {
+export class CarComponent implements
+  OnInit,
+  OnChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy
+{
+
 
   //Можно из html-шаблона передавать само свойство carItem,
   // а можно его указать в alias(названия того параметра, который мы ожидаем) аннотации @Input
   // @Input() carItem: { name: string, year: number }
   @Input('carItem') car: { name: string, model: string, year: number, price: number }
+  @Input('name') name: string;
+
   @ContentChild('carHeading') carHeading:ElementRef;
 
+  constructor(){
+    console.log('constructor');
+  }
+
+  ngOnInit(): void {
+    console.log('ngOnInit');
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('ngOnChanges');
+  }
+
+  ngDoCheck(): void {
+    console.log('ngDoCheck');
+  }
+
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit');
+  }
+
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked');
+  }
+
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit');
+  }
+
+  ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked');
+  }
+
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy');
+  }
 }
--- src/app/cars/cars.component.ts
+++ src/app/cars/cars.component.ts
@@ -12,17 +12,25 @@ export class CarsComponent {
   cars: [{ name: string, year: number }] = [
     {
       name: 'Ford', year: 2013
-    }, {
-      name: 'Audi', year: 2011
-    }, {
-      name: 'BMW', year: 2017
-    }, {
-      name: 'MAN', year: 2005
-    }, {
-      name: 'Mazda', year: 2013
+    // }, {
+    //   name: 'Audi', year: 2011
+    // }, {
+    //   name: 'BMW', year: 2017
+    // }, {
+    //   name: 'MAN', year: 2005
+    // }, {
+    //   name: 'Mazda', year: 2013
     }];
 
   updateCarList(car: {name:string, year: number}){
     this.cars.push(car);
+  }
+
+  changeCarName(){
+    this.cars[0].name = 'New year car!';
+  }
+
+  deleteCar(){
+    this.cars.splice(0, 1);
   }
 }
