ead5e5f5b4b3928914b2f7808c542e388533e629
ss.kovalevskiy <ss.kovalevskiy@yandex.ru>
2017-11-18 01:36:46 +0300

    Добавили рендерер (private renderer: Renderer2)в качестве аналога 'this.element.nativeElement.style.*' поскольку рендерер корректно распознается любой платформой, а не только браузером

--- src/app/directives/background.directive.ts
+++ src/app/directives/background.directive.ts
@@ -1,15 +1,16 @@
-import {Directive, ElementRef, OnInit} from "@angular/core";
+import {Directive, ElementRef, OnInit, Renderer2} from "@angular/core";
 
 @Directive({
   selector: '[appBackground]'
 })
 export class BackgroundDirective implements OnInit{
 
-  constructor(private element: ElementRef) {
+  constructor(private element: ElementRef, private renderer: Renderer2) {
 
   }
 
   ngOnInit(): void {
     this.element.nativeElement.style.backgroundColor = 'violet';
+    this.renderer.setStyle(this.element.nativeElement, 'text-align', 'center');
   }
 }
