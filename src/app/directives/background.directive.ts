e723c3631acb122085fe9d972a55144545b79ea4
ss.kovalevskiy <ss.kovalevskiy@yandex.ru>
2017-11-18 02:45:37 +0300

    Добавляем аннотацию @HostBinding, которая позволяет сократить код обращения к полям элемента

--- src/app/directives/background.directive.ts
+++ src/app/directives/background.directive.ts
@@ -1,9 +1,11 @@
-import {Directive, ElementRef, HostListener, OnInit, Renderer2} from "@angular/core";
+import {Directive, ElementRef, HostBinding, HostListener, OnInit, Renderer2} from "@angular/core";
 
 @Directive({
   selector: '[appBackground]'
 })
 export class BackgroundDirective implements OnInit {
+
+  @HostBinding('style.border') border: string;
 
   constructor(private element: ElementRef, private renderer: Renderer2) {
 
@@ -14,16 +16,22 @@ export class BackgroundDirective implements OnInit {
     this.renderer.setStyle(this.element.nativeElement, 'text-align', 'center');
 
     const {nativeElement} = this.element;
-    this.renderer.addClass(nativeElement, 'white-text')
+    this.renderer.addClass(nativeElement, 'white-text');
+    this.border = 'double';
   }
 
   @HostListener('mouseenter', ['$event']) mouseEnter(event: Event){
     console.log(event);
     this.renderer.setStyle(this.element.nativeElement, 'background-color', 'yellow');
+    this.border = 'solid';
   }
+
 
   @HostListener('mouseleave', ['$event']) mouseLeave(event: Event){
     console.log(event);
     this.renderer.setStyle(this.element.nativeElement, 'background-color', 'violet');
+    this.border = 'double';
   }
+
+
 }
