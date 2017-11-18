f27acb05a5a76f217d821af84bad71aadba1a17a
ss.kovalevskiy <ss.kovalevskiy@yandex.ru>
2017-11-18 21:49:15 +0300

    Вариант изменения стилей элемента с помощью директив @Input, получающих значения свойств из html-макета элемента

--- src/app/directives/my-directive.directive.ts
+++ src/app/directives/my-directive.directive.ts
@@ -1,10 +1,27 @@
-import { Directive } from '@angular/core';
+import {Directive, HostBinding, HostListener, Input, OnInit} from '@angular/core';
 
 @Directive({
   selector: '[appMyDirective]'
 })
-export class MyDirectiveDirective {
+export class MyDirectiveDirective implements OnInit {
 
-  constructor() { }
+  @Input() hoverColor: string;
+  @Input() defaultColor: string;
+  @HostBinding('style.backgroundColor') background: string;
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    this.background = this.defaultColor
+  }
+
+  @HostListener('mouseenter') mouseEnter() {
+    this.background = this.hoverColor;
+  }
+
+  @HostListener('mouseleave') mouseLeave() {
+    this.background = this.defaultColor;
+  }
 
 }
