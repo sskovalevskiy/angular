209cacca4a1dc0add7ec3c1d41fabc5122cd858c
ss.kovalevskiy <ss.kovalevskiy@yandex.ru>
2017-11-19 23:18:45 +0300

    Пример использования async pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
-
+import 'rxjs/Rx';
+import {Observable} from 'rxjs/Observable'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,7 +10,7 @@ export class AppComponent {
   title = 'app';
   items = [1, 2, 3, 4, 5];
   current = 1;
-  searchCar:string = '';
+  searchCar: string = '';
   cars = [
     {name: 'Ford', year: 2015},
     {name: 'Audi', year: 2013},
@@ -17,6 +18,8 @@ export class AppComponent {
     {name: 'MAN', year: 2016},
     {name: 'Mazda', year: 2017}
   ];
+
+  asyncTitle = Observable.of('Async title 3 seconds').delay(3000);
 
   onClick(number: number) {
     this.current = number;
